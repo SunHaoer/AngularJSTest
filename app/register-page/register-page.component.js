e5c7ead519defca46921c88a28162beb8dec054a
SunHao <2457302636@qq.com>
2019-01-21 10:35:32 +0800

    添加了想服务器端发送保存数据的http请求

--- app/register-page/register-page.component.js
+++ app/register-page/register-page.component.js
@@ -2,11 +2,30 @@ angular.
 module('registerPage').
 component('registerPage', {
     templateUrl: 'register-page/register-page.template.html',
-    controller: ['PhoneMsg', function RegisterPageCtrl(PhoneMsg) {
+    controller: ['PhoneMsg', '$http', function RegisterPageCtrl(PhoneMsg, $http) {
         this.phone = new PhoneMsg();
         this.brandRegex = '\\d+';
         this.brandList = ['SAMSUNG', 'IPHONE', 'OPPO', 'HUAWEI'];
         this.ps = PhoneMsg.query();
+				
+				this.save = function() {
+					alert('send to java');
+					var phone = this.phone;
+					$http({
+						method: 'POST',
+						params: ({
+							brand: phone.brand,
+							type: phone.type,
+							productNo: phone.productNo
+						}),
+						url: 'http://localhost:8120/phone/save',
+						headers: { 'Content-Type': 'application/json' } 
+					}).then(function success(response) {
+						alert(response.data.result);
+					}, function error(response) {
+						alert("error");
+					});
+				}
 
        
         this.submitMsg = function () {
