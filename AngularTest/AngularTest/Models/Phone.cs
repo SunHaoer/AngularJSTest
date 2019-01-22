aa146452ae9293de8f854ef0f26e5c019bc00770
yuukachen <45988831+yuukachen@users.noreply.github.com>
2019-01-22 13:50:25 +0800

    1

---
+++ AngularTest/AngularTest/Models/Phone.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AngularTest.Models
+{
+    public class Phone
+    {
+        public long Id { get; set; }
+        public string PhoneUser { get; set; }
+        public string Brand { get; set; }
+        public string Type { get; set; }
+        public long ProductNo { get; set; }
+        public DateTime InputDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public DateTime DeleteDate { get; set; }
+        public string AbandonReason { get; set; }
+    }
+}
