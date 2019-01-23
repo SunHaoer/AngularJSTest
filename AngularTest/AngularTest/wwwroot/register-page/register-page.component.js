9e446c1b7b0490e261c2eb0b3bd6f611efce64e5
SunHao <2457302636@qq.com>
2019-01-23 17:59:10 +0800

    进行了部分修改

--- AngularTest/AngularTest/wwwroot/register-page/register-page.component.js
+++ AngularTest/AngularTest/wwwroot/register-page/register-page.component.js
@@ -4,7 +4,10 @@ angular.
         templateUrl: 'register-page/register-page.template.html',
         controller: ['$scope', '$http', function RegisterPageCtrl($scope, $http) {
             $scope.brandRegex = '\\d+';
-            $scope.flag = false;
+
+            $scope.isRegister = true;
+            $scope.isReplace = false;
+
             /**
              * 获取所有手机品牌
              * */
@@ -19,9 +22,9 @@ angular.
                     for (var i = 0; i < list.length; i++) {
                         $scope.brandList.push(list[i]["brand"]);
                     }
-
+                    
                 }, function error(response) {
-                    alert("brand error");
+                    alert("error");
                 });
             }
             $scope.getBrandAll();
@@ -40,26 +43,20 @@ angular.
                     headers: { 'Content-Type': 'application/json' }
                 }).then(function success(response) {
                     var list = response.data;
+                    console.log(list);
                     $scope.typeList = [];
                     for (var i = 0; i < list.length; i++) {
                         $scope.typeList.push(list[i]["type"]);
                     }
                 }, function error(response) {
-                    alert("type error");
+                    alert("error");
                 });
             }
 
             /**
              * 根据型号获取保质期
              * */
-            $scope.getYearByType = function () {
-                var typeFlag = $scope.phone.type;
-                if (typeFlag != "none") {
-                    console.log(typeFlag);
-                    $scope.flag = true;
-                } else {
-                    $scope.flag = false;
-                }
+            $scope.getYearByType = function() {
                 $http({
                     method: 'GET',
                     params: ({
@@ -69,7 +66,9 @@ angular.
                     headers: { 'Content-Type': 'application/json' }
                 }).then(function success(response) {
                     $scope.phone.life = response.data;
+                    //alert('+' + $scope.phone.life);
                 }, function error(response) {
+                    alert("error");
                 });
             }
 
@@ -77,16 +76,19 @@ angular.
              * 日期格式化
              * */
             $scope.formatDate = function () {
-                var inputDate = $scope.inputDate;
+                var inputDate = $scope.phone.inputDate;
                 var year = inputDate.getFullYear();
                 var month = inputDate.getMonth() + 1;
                 if (month < 10) month = '0' + month;
                 var date = inputDate.getDate();
                 if (date < 10) date = '0' + date;
-                var startDate = year + '-' + month + '-' + date;
-                var endDate = (year + $scope.phone.life) + '-' + month + '-' + date;
+                var startDate = year + '' + month + '' + date;
+                var endDate = (year + $scope.phone.life) + '' + month + '' + date;
                 $scope.phone.startDate = startDate;
                 $scope.phone.endDate = endDate;
+                //alert('-' + $scope.phone.life);
+                //alert(startDate);
+                //alert(endDate);
             }
 
             /**
@@ -97,11 +99,11 @@ angular.
                 $http({
                     method: 'POST',
                     params: ({
-                        phoneUser: phone.phoneUser,
+                        phoneUser: 'Dillon',
                         brand: phone.brand,
                         type: phone.type,
                         productNo: phone.productNo,
-                        startDate: phone.startDate,
+                        inputDate: phone.startDate,
                         endDate: phone.endDate
                     }),
                     url: '/api/DoubleCheck/SetTempPhone',
@@ -109,12 +111,21 @@ angular.
                 }).then(function success(response) {
                     alert(response.data);
                 }, function error(response) {
-                    alert("save error");
+                    alert("error");
                 });
             }
 
 
-            
+            $scope.submitMsg = function () {
+                console.log($scope.phone);
+                var json = JSON.stringify($scope.phone);
+                console.log(json);
+                PhoneMsg.save(json, function (data) {
+                    json = data;
+                }, function (resp) {
+                    console.log('error');
+                });
+            }
 
 
         }]
