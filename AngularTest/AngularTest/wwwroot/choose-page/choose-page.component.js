d57f25fd0d6041af66ec087855ec6c521f674e69
SunHao <2457302636@qq.com>
2019-01-23 18:21:18 +0800

    整合了部分代码

--- AngularTest/AngularTest/wwwroot/choose-page/choose-page.component.js
+++ AngularTest/AngularTest/wwwroot/choose-page/choose-page.component.js
@@ -4,43 +4,43 @@ component('choosePage',{
     templateUrl:'choose-page/choose-page.template.html',
     controller: ['$scope', '$http','$location', function ChoosePageCtrl($scope, $http, $location) {
 
-        var brand2 = "OPPO";
-        var phone = this.phone;
-        $http({
-            method: 'GET',
-            params: ({
-            brand: brand2,
-        }),
-            url: '/api/BrandTypeModels/GetTypeByBrand',
-            headers: { 'Content-Type': 'application/json' }
-        }).then(function success(response) {
-            var typeList = response.data;
-            console.log(typeList);
-            var list = [];
-            for (var i = 0; i < typeList.length; i++) {
-                list.push(typeList[i]["type"]);
-            }
-            console.log(list);
-        }, function error(response) {
-            alert("error");
-        });
+        //var brand2 = "OPPO";
+        //var phone = this.phone;
+        //$http({
+        //    method: 'GET',
+        //    params: ({
+        //    brand: brand2,
+        //}),
+        //    url: '/api/BrandTypeModels/GetTypeByBrand',
+        //    headers: { 'Content-Type': 'application/json' }
+        //}).then(function success(response) {
+        //    var typeList = response.data;
+        //    console.log(typeList);
+        //    var list = [];
+        //    for (var i = 0; i < typeList.length; i++) {
+        //        list.push(typeList[i]["type"]);
+        //    }
+        //    console.log(list);
+        //}, function error(response) {
+        //    alert("error");
+        //});
 
         this.replace = function (oldId) {
+            
+            //$scope.phone = {
+            //    'id': 1,
+            //    'phoneUser': 'Hujun',
+            //    'brand': 'OPPO',
+            //    'type': 'K1',
+            //    'productNo': '123456',
+            //    'startDate': '20190123',
+            //    'endDate': '',
+            //    'deleteDate': '',
+            //    'abandonReson': '',
+            //    'state': 0
+            //}
 
-            $scope.phone = {
-                'id': 1,
-                'phoneUser': 'Hujun',
-                'brand': 'OPPO',
-                'type': 'K1',
-                'productNo': '123456',
-                'startDate': '20190123',
-                'endDate': '',
-                'deleteDate': '',
-                'abandonReson': '',
-                'state': 0
-            }
-
-            console.log(oldId);
+            //console.log(oldId);
 
             $http({
                 method: 'Post',
