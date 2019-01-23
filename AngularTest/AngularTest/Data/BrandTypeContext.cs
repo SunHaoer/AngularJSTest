456fa1628b38f8a47e888c7fa6f2850702c91da5
SunHao <2457302636@qq.com>
2019-01-23 19:37:49 +0800

    修改了部分代码

---
+++ AngularTest/AngularTest/Data/BrandTypeContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AngularTest.Models
+{
+    public class BrandTypeContext : DbContext
+    {
+        public BrandTypeContext(DbContextOptions<BrandTypeContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BrandType> BrandTypes { get; set; }
+    }
+}
